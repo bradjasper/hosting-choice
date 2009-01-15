084cdbc324630a608942c205e4e346bd50cf1d79
Brad Jasper <bjasper@gmail.com>
2009-01-14 18:20:19 -0600

    fcgi tweaks

--- settings.py
+++ settings.py
@@ -41,7 +41,7 @@ MEDIA_ROOT = '/var/www/hosting-choice.com/static/images/'
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = '/images/'
+MEDIA_URL = '/media/images/'
 
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
