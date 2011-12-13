b9bfa4972e5658b3bf2e83e20465ec37fc6d6c5c
Brad Jasper <bjasper@gmail.com>
2011-12-13 01:31:56 -0500

    change pass

--- settings.py
+++ settings.py
@@ -13,7 +13,7 @@ MANAGERS = ADMINS
 DATABASE_ENGINE = 'mysql'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
 DATABASE_NAME = 'hostingchoice'             # Or path to database file if using sqlite3.
 DATABASE_USER = 'hc'             # Not used with sqlite3.
-DATABASE_PASSWORD = 'LJ12oiu23.sl2'         # Not used with sqlite3.
+DATABASE_PASSWORD = 'pass123'         # Not used with sqlite3.
 DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
 DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
 
